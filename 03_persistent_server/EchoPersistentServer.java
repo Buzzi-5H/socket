85d31836248b51f3623c430849ca1817d4b591a5
Filippo Biondi <filibiondi2000@gmail.com>
2025-10-13 15:07:02 +0200

    Added solutions to exercise 3

--- 03_persistent_server/EchoPersistentServer.java
+++ 03_persistent_server/EchoPersistentServer.java
@@ -1,10 +1,31 @@
-// Aggiungi gli import necessari
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class EchoServerPersistent {
+public class EchoPersistentServer {
   public static void main(String[] args) {
+      try (ServerSocket serverSocket = new ServerSocket(12345)) {
+          System.out.println("Server in attesa di connessioni...");
+          while (true) {
+              try (Socket clientSocket = serverSocket.accept()) {
+                  System.out.println("Client connesso: " + clientSocket.getInetAddress());
 
-    // Rispondi al client con lo stesso messaggio ricevuto e chiudi la connessione.
-    // Rimani sempre in attesa di nuovi client
+                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                  PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
+                  String messaggio = in.readLine();
+                  System.out.println("Ricevuto: " + messaggio);
+
+                  out.println(messaggio);
+                  System.out.println("Risposto con: " + messaggio);
+              }
+          }
+      } catch (Exception e) {
+          System.err.println("Errore nel server: " + e.getMessage());
+      }
+
+      System.out.println("Server terminato.");
   }
 }
